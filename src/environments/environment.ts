61eb82c6db3ac61fbe6aa2596bd25a25b7821743
ztimson <ztimson@psdrcs.com>
2019-07-09 12:23:53 -0400

    Added continues integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDFtvCY6nH_HUoTBNf_5b-E8nRweSLYtxE",
+    authDomain: "mapalliance-ab38a.firebaseapp.com",
+    databaseURL: "https://mapalliance-ab38a.firebaseio.com",
+    projectId: "mapalliance-ab38a",
+    storageBucket: "",
+    messagingSenderId: "443222036375",
+    appId: "1:443222036375:web:2e2906e3ca49aa63"
+  },
   production: false
 };
 
