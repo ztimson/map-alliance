8e0a2e966f6d14475bb717416682afcc67d3e0ad
ztimson <zaktimson@gmail.com>
2019-08-24 20:57:39 -0400

    more dark theme 😈

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -5,7 +5,6 @@ import {version} from '../../../../package.json';
 @Component({
     selector: 'toolbar',
     templateUrl: 'toolbar.component.html',
-    styleUrls: ['toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit, AfterViewInit {
     @Input() menuItems: ToolbarItem[];
