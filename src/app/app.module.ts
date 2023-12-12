eed553d866024f37179c988690ce9e4699df9fa8
ztimson <zaktimson@gmail.com>
2023-12-11 22:21:59 -0500

    Fixed service workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from "@angular/core";
+import {isDevMode, NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ServiceWorkerModule} from "@angular/service-worker";
 import {ClickOutsideModule} from "ng-click-outside";
 import {ColorPickerModule} from "ngx-color-picker";
-import {environment} from '../environments/environment';
+import {AppComponent} from './app.component';
 import {AppRouting} from './app.routing';
 import {CalibrateComponent} from "./components/calibrate/calibrate.component";
 import {ColorPickerDialogComponent} from "./components/colorPickerDialog/colorPickerDialog.component";
@@ -16,7 +16,6 @@ import {PermissionsComponent} from "./components/permissions/permissions.compone
 import {StarrySkyComponent} from "./components/starrySky/starrySky.component";
 import {ToolbarComponent} from "./components/toolbar/toolbar.component";
 import {MaterialModule} from "./material.module";
-import {AppComponent} from './app.component';
 import {HomeComponent} from "./views/home/home.component";
 import {MapComponent} from "./views/map/map.component";
 
@@ -42,8 +41,11 @@ import {MapComponent} from "./views/map/map.component";
 		ClickOutsideModule,
 		ColorPickerModule,
 		MaterialModule,
-		ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			registrationStrategy: 'registerWhenStable:30000' // when stable or after 30 seconds
+		})
 	],
 	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
